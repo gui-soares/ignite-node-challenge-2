c3f8a3951b9f4333a91bca6ed4a4f1ee0c781b66
dev-guilherme-soares <guilherme.soares@zukk.com.br>
2021-04-01 00:59:55 -0300

    challenge completed

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,17 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("user not found");
+    }
+
+    if (!user.admin) {
+      throw new Error("only admins can get users list");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
